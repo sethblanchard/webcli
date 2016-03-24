1ca8c8433419272b296be001217939ddb46205c0
Seth Blanchard <blanchard.seth@gmail.com>
2016-03-24 19:19:19 -0400

    updating app.js

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
- var socket = io('http://localhost:7123');
-  socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-  });
+var socket = io('http://localhost:7123');
+socket.on('from_cli', function (data) {
+
+	//globally scoped eval so we can get at things like test... 
+	var geval = eval;
+
+var result = { err: null, result: null };
+
+try{
+	console.log(data.line);
+	result.result = geval(data.line);
+	console.log(result.result);
+}catch(exp){
+	console.error(exp);
+	result.err = exp.toString();
+}
+
+//will need to be a callback...
+socket.emit('from_browser', result);
+});
